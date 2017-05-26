72e68a5ecfcf7b8f92450aca60d1db00c2d33a47
Mirela Bumb(Muncaciu) <ingmmccia@yahoo.com>
2017-05-26 14:43:08 +0300

    the list with movies

---
+++ constants/movieList.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Mirela Bumb(Muncaciu) on 5/26/2017.
+ */
+import React from 'react';
+
+export const movies = [
+    {
+        name:"The Forest",
+        year:2016,
+        date:"January 8",
+        directors:"Jason Zada"
+    },
+    {
+        name:"The Masked Saint",
+        year:2016,
+        date:"January 8",
+        directors:"Warren P. Sonoda"
+    },
+    {
+        name:"13 Hours",
+        year:2016,
+        date:"January 15",
+        directors:"Michael Bay"
+    },
+    {
+        name:"Ride Along 2",
+        year:2016,
+        date:"January 15",
+        directors:"Tim Story"
+    },
+    {
+        name:"Ip Man 3",
+        year:2016,
+        date:"January 22",
+        directors:"Wilson Yip"
+    },
+    {
+        name:"Lazer Team",
+        year:2016,
+        date:"January 27",
+        directors:"Matt Hullum"
+    },
+    {
+        name:"Snowtime",
+        year:2016,
+        date:"February 19",
+        directors:"Jean-François Pouliot, François Brisson"
+    },
+    {
+        name:"Zootopia",
+        year:2016,
+        date:"March 4",
+        directors:"Byron Howard, Rich Moore, Jared Bush"
+    },
+    {
+        name:"Miles Ahead",
+        year:2016,
+        date:"April 1",
+        directors:"Don Cheadle"
+    },
+    {
+        name:"Ratchet And Clank",
+        year:2016,
+        date:"April 29",
+        directors:"Jericca Cleland"
+    },
+    {
+        name:"Absolutely Anything",
+        year:2017,
+        date:"May 12",
+        directors:"Terry Jones"
+    },
+    {
+        name:"Diary of a Wimpy Kid The Long Haul",
+        year:2017,
+        date:"May 19",
+        directors:"David Bowers"
+    },
+    {
+        name:"3 Idiotas",
+        year:2017,
+        date:"June 2",
+        directors:"Carlos Bolado"
+    },
+    {
+        name:"Wonder Woman",
+        year:2017,
+        date:"June 2",
+        directors:"Patty Jenkins"
+    },
+    {
+        name:"Transformers 5",
+        year:2017,
+        date:"June 21",
+        directors:"Michael Bay"
+    }
+
+];
