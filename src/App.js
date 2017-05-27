0a66017c6aaf68f3dfaff0cc72550c60ee410575
Mirela Bumb(Muncaciu) <ingmmccia@yahoo.com>
2017-05-27 17:50:13 +0300

    try to find the problem - but still not working

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import {movies} from  './constants/movieList.js';
 import SearchBox from './components/Search.js';
 
+console.log('return form searchBox', SearchBox);
+
 function searchingFor (term) {
     return function (y) {
-        return y.name.toLowerCase().includes(term.toLowerCase()) || !term;
+        console.log('this is y', y);
+        return !(!y.name.toLowerCase().includes(term.toLowerCase()) && term);
     }
 }
 
@@ -18,26 +21,27 @@ class App extends Component {
         };
     }
     render() {
+        console.log('I was triggered during app render');
         const {term,movies} = this.state;
+        console.log('the state before search',this.state);
+        console.log('the movies is',movies);
+        console.log('the term is', term+'is empty?');
         return (
             <div className="container">
                 <SearchBox />
-
                 {
-                    movies.filter(searchingFor(term)).map(movie => {
-                        return(
-                            <div key={movie.id} className="movies">
-                                <div className="col-md-6">
-                                    <h1 className="well">{movie.name}</h1>
-                                    <ul className="list-group">
-                                        <li className="list-group-item">{movie.year}</li>
-                                        <li className="list-group-item">{movie.date}</li>
-                                        <li className="list-group-item">Directed by: {movie.directors}</li>
-                                    </ul>
-                                </div>
+                    movies.filter(searchingFor(term)).map(movie =>
+                        <div key={movie.id} className="movies">
+                            <div className="col-md-6">
+                                <h1 className="well">{movie.name}</h1>
+                                <ul className="list-group">
+                                    <li className="list-group-item">{movie.year}</li>
+                                    <li className="list-group-item">{movie.date}</li>
+                                    <li className="list-group-item">Directed by: {movie.directors}</li>
+                                </ul>
                             </div>
-                        )
-                    })
+                        </div>
+                    )
                 }
             </div>
         );
