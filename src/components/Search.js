4be669f12b7b7fbff8f0b907b728ab745788adea
Mirela Bumb(Muncaciu) <ingmmccia@yahoo.com>
2017-05-27 14:18:28 +0300

    import the css + console logs to find problems

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,27 +2,32 @@
  * Created by Mirela Bumb on 5/26/2017.
  */
 import React,{Component} from 'react';
+import '../search.css';
 
 class SearchBox extends Component {
     constructor (props){
         super(props);
+        this.state = {term:''};
         this.searchHandler = this.searchHandler.bind(this);
-        this.state = {
-            term:''
-        }
     }
     searchHandler(ev){
-        this.setState({term: ev.target.value})
-    }
+        console.log('the event ..');
+        this.setState({term: ev.target.value});
+        console.log('after setstate');
+        alert('The new value of input is:' + this.state.term);
 
+    }
     render (){
+        console.log('I was triggered during render');
         return (
             <div className="SearchBox">
                 <form>
-                    <input className="form-control form-control-lg" placeholder="Search" type="text" onChange={this.searchHandler} value={this.term}/>
+                    <label>Looking for a specific movie?</label>
+                    <input className="form-control form-control-lg" placeholder="Search" type="text" onChange={this.searchHandler} value={this.state.term} />
                 </form>
             </div>
         )
+
     }
 }
 export default SearchBox;
