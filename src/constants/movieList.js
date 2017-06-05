cb57fb2ed7d9c2c5f1e03921c49963699d4df667
Mirela Bumb(Muncaciu) <ingmmccia@yahoo.com>
2017-06-05 17:09:45 +0300

    delete ids

--- src/constants/movieList.js
+++ src/constants/movieList.js
@@ -5,70 +5,60 @@ import React from 'react';
 
 export const movies = [
     {
-        id:1,
         name:"The Forest",
         year:2016,
         date:"January 8",
         directors:"Jason Zada"
     },
     {
-        id:2,
         name:"The Masked Saint",
         year:2016,
         date:"January 8",
         directors:"Warren P. Sonoda"
     },
     {
-        id:3,
         name:"13 Hours",
         year:2016,
         date:"January 15",
         directors:"Michael Bay"
     },
     {
-        id:4,
         name:"Ride Along 2",
         year:2016,
         date:"January 15",
         directors:"Tim Story"
     },
     {
-        id:5,
         name:"Ip Man 3",
         year:2016,
         date:"January 22",
         directors:"Wilson Yip"
     },
     {
-        id:6,
         name:"Lazer Team",
         year:2016,
         date:"January 27",
         directors:"Matt Hullum"
     },
     {
-        id:7,
         name:"Snowtime",
         year:2016,
         date:"February 19",
         directors:"Jean-François Pouliot, François Brisson"
     },
     {
-        id:8,
         name:"Zootopia",
         year:2016,
         date:"March 4",
         directors:"Byron Howard, Rich Moore, Jared Bush"
     },
     {
-        id:9,
         name:"Miles Ahead",
         year:2016,
         date:"April 1",
         directors:"Don Cheadle"
     },
     {
-        id:10,
         name:"Ratchet And Clank",
         year:2016,
         date:"April 29",
@@ -82,32 +72,27 @@ export const movies = [
         directors:"Terry Jones"
     },
     {
-        id:12,
         name:"Diary of a Wimpy Kid The Long Haul",
         year:2017,
         date:"May 19",
         directors:"David Bowers"
     },
     {
-        id:13,
         name:"3 Idiotas",
         year:2017,
         date:"June 2",
         directors:"Carlos Bolado"
     },
     {
-        id:14,
         name:"Wonder Woman",
         year:2017,
         date:"June 2",
         directors:"Patty Jenkins"
     },
     {
-        id:15,
         name:"Transformers 5",
         year:2017,
         date:"June 21",
         directors:"Michael Bay"
     }
-
 ];
